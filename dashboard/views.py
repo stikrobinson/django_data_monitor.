edffd5414f5640182e2813290469646711ed95a2
stikrobinson <148508110+stikrobinson@users.noreply.github.com>
2025-08-04 19:56:44 +0000

    guia23

---
+++ dashboard/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import requests
+from django.conf import settings
+
+def dashboard(request):
+
+    response = requests.get(settings.API_URL)  # URL de la API
+    posts = response.json()  # Convertir la respuesta a JSON
+
+    # Número total de respuestas
+    total_responses = len(posts)
+
+    data = {
+        'title': "Landing Page' Dashboard",
+        'total_responses': total_responses,
+    }
+
+    return render(request, 'dashboard/index.html', data)
