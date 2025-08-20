00cf5fde8e7fd75633adbbd7a40d6adc1c9bc83b
stikrobinson <148508110+stikrobinson@users.noreply.github.com>
2025-08-20 19:39:18 +0000

    setting

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, STATIC_URL),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 STATIC_ROOT = BASE_DIR / 'assets'
