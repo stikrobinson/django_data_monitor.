6e3a9bd0d587f7b20eeca5687e2a496e69f92b2f
stikrobinson <148508110+stikrobinson@users.noreply.github.com>
2025-08-21 03:48:06 +0000

    host correction

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -35,7 +35,7 @@ CSRF_TRUSTED_ORIGINS = [
   "http://127.0.0.1:8000",
 ]
 
-ALLOWED_HOSTS = ['.up.railway.app']
+ALLOWED_HOSTS = ['https://*.up.railway.app']
 
 
 
@@ -87,12 +87,8 @@ WSGI_APPLICATION = "backend_analytics_server.wsgi.application"
 
 DATABASES = {
     "default": {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE'),
-        'USER': os.environ.get('MYSQLUSER'),
-        'PASSWORD': os.environ.get('MYSQLPASSWORD'),
-        'HOST': os.environ.get('MYSQLHOST'),
-        'PORT': os.environ.get('MYSQLPORT'),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
