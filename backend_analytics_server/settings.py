4de5561c4ef5aae86d6bc6cee5ac738ed702b99a
stikrobinson <148508110+stikrobinson@users.noreply.github.com>
2025-08-21 00:44:50 +0000

    coreccion whitenoise

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -140,11 +140,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
